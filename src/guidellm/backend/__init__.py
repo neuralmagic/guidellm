39ffcb314b0845f60d417b690c9f600f6ebd687e
Dmytro Parfeniuk <parfeniukinik@gmail.com>
2024-08-30 11:57:29 +0300

    🚚 DeepsparseBackend is moved to a another module `backend.DeepsparseBackend` -> `backend.deepsparse.DeepsparseBackend`

--- src/guidellm/backend/__init__.py
+++ src/guidellm/backend/__init__.py
@@ -1,12 +1,3 @@
 from .base import Backend, BackendEngine, BackendEnginePublic, GenerativeResponse
-from .deepsparse.backend import DeepsparseBackend
-from .openai import OpenAIBackend
 
-__all__ = [
-    "Backend",
-    "BackendEngine",
-    "BackendEnginePublic",
-    "GenerativeResponse",
-    "OpenAIBackend",
-    "DeepsparseBackend",
-]
+__all__ = ["Backend", "BackendEngine", "BackendEnginePublic", "GenerativeResponse"]
--- src/guidellm/backend/deepsparse/__init__.py
+++ src/guidellm/backend/deepsparse/__init__.py
@@ -1,5 +1,6 @@
 """
 This package encapsulates the "Deepsparse Backend" implementation.
+
 ref: https://github.com/neuralmagic/deepsparse
 
 The `deepsparse` package supports Python3.6..Python3.11,
@@ -7,14 +8,13 @@ when the `guidellm` start from Python3.8.
 
 Safe range of versions is Python3.8..Python3.11
 for the Deepsparse Backend implementation.
+
+In the end ensure that the `deepsparse` package is installed.
 """
 
 from guidellm.utils import check_python_version, module_is_available
 
-# Ensure that python is in valid range
 check_python_version(min_version="3.8", max_version="3.11")
-
-# Ensure that deepsparse is installed
 module_is_available(
     module="deepsparse",
     helper=(
@@ -22,3 +22,7 @@ module_is_available(
         "Please try `pip install -e '.[deepsparse]'`"
     ),
 )
+
+from .backend import DeepsparseBackend
+
+__all__ = ["DeepsparseBackend"]
