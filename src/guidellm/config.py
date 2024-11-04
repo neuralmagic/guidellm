cb1f244ac5ce15dad230bbf84f541868b3ffa393
Alexandre Marques <alexandre@neuralmagic.com>
2024-11-04 19:10:01 +0000

    Add url used to download images from for emulated requests

--- src/guidellm/config.py
+++ src/guidellm/config.py
@@ -90,6 +90,7 @@ class EmulatedDataSettings(BaseModel):
             "force_new_line_punctuation": True,
         }
     )
+    image_source: List[str] = "https://www.gutenberg.org/cache/epub/1342/pg1342-images.html"
 
 
 class OpenAISettings(BaseModel):
