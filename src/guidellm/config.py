1e21612687a3b3408c8e18ab787776efb0bef8c5
Alexandre Marques <alexandre@neuralmagic.com>
2024-12-06 21:54:52 +0000

    Adds aiohttp backend

--- src/guidellm/config.py
+++ src/guidellm/config.py
@@ -106,6 +106,9 @@ class OpenAISettings(BaseModel):
     max_output_tokens: int = 16384
 
 
+class AiohttpSettings(OpenAISettings):
+    pass
+
 class ReportGenerationSettings(BaseModel):
     """
     Report generation settings for the application
@@ -154,6 +157,7 @@ class Settings(BaseSettings):
     preferred_output_tokens_source: Optional[Literal["backend", "local"]] = None
     preferred_backend: Literal["openai"] = "openai"
     openai: OpenAISettings = OpenAISettings()
+    aiohttp: AiohttpSettings = AiohttpSettings()
 
     # Report settings
     report_generation: ReportGenerationSettings = ReportGenerationSettings()
