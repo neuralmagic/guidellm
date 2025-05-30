7f536c72ae8e29805b4d850148c09cbd355954b0
dalthecow <dalcowboiz@gmail.com>
2025-05-30 12:57:56 -0400

    update versions

--- src/ui/.next/static/chunks/app/layout-e21b05184451c9a4.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{1498:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,3139,23))},3139:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[678,587,315,358],()=>s(1498)),_N_E=e.O()}]);
--- src/ui/.next/static/chunks/pages/_error-b8c2e08355edc3b4.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[731],{1851:(_,n,e)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return e(8145)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[636,593,792],()=>n(1851)),_N_E=_.O()}]);
