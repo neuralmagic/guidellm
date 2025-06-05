83d6f44ba695891acc3905885dedd6ec8cc2b5e0
dalthecow <dalcowboiz@gmail.com>
2025-06-05 18:26:06 -0400

    add test coverage setup

--- jest.config.js
+++ jest.config.js
@@ -5,16 +5,21 @@ const createJestConfig = nextJest({
 });
 
 const customJestConfig = {
-  collectCoverage: false,
-  collectCoverageFrom: ['tests'],
-  coverageDirectory: './.meta',
-  coverageReporters: ['json-summary'],
+  collectCoverage: true,
+  collectCoverageFrom: ['./src/ui/**/*.{ts,tsx}'],
+  coverageDirectory: './coverage',
+  coverageProvider: 'v8',
+  coverageReporters: ['json', 'text-summary', 'lcov'],
   moduleFileExtensions: ['ts', 'tsx', 'js'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
   },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testEnvironment: 'jest-environment-jsdom',
+  testMatch: [
+    '<rootDir>/tests/ui/unit/**/*.(test|spec).{ts,tsx,js,jsx}',
+    '<rootDir>/tests/ui/integration/**/*.(test|spec).{ts,tsx,js,jsx}',
+  ],
 };
 
 module.exports = createJestConfig(customJestConfig);
