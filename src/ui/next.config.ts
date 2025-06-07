ec10b926ebbd035c84164d371212788d1fb7ce50
dalthecow <dalcowboiz@gmail.com>
2025-06-06 22:29:05 -0400

    add any type to next config

--- src/ui/next.config.ts
+++ src/ui/next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   output: 'export',
   webpack(config) {
     // Grab the existing rule that handles SVG imports
-    const fileLoaderRule = config.module.rules.find((rule) =>
+    const fileLoaderRule = config.module.rules.find((rule: any) =>
       rule.test?.test?.('.svg')
     );
 
