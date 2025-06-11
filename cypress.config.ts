c02f70151dff6cbaa5d6ec395ee2bba0ab93ebc1
dalthecow <dalcowboiz@gmail.com>
2025-06-11 15:13:11 -0400

    update config comments

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,6 @@ export default defineConfig({
   e2e: {
     specPattern: 'tests/ui/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     supportFile: 'tests/ui/cypress/support/e2e.ts',
-    baseUrl: 'http://localhost:3000', // optional, but good practice
+    baseUrl: 'http://localhost:3000', // optional
   },
 });
