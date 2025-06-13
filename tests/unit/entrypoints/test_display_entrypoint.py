18d58971fc892da53fcc5ae16a9c51876b41fb90
Jared O'Connell <joconnel@redhat.com>
2025-06-13 12:50:34 -0400

    Fix linter error and exclude test assets from linting

--- tests/unit/entrypoints/test_display_entrypoint.py
+++ tests/unit/entrypoints/test_display_entrypoint.py
@@ -32,7 +32,7 @@ def test_display_entrypoint_yaml(capfd, get_test_asset_dir):
 
 
 def generic_test_display_entrypoint(filename, capfd, get_test_asset_dir):
-    os.environ['COLUMNS'] = "120"  # CLI output depends on terminal width.
+    os.environ["COLUMNS"] = "120"  # CLI output depends on terminal width.
     asset_dir = get_test_asset_dir()
     display_benchmarks_report(asset_dir / filename)
     out, err = capfd.readouterr()
