6f6dda826d10419508ed6251a6f4145f47762f3f
dalthecow <dalcowboiz@gmail.com>
2025-06-24 13:06:38 -0400

    add img handling config to hopefully reduce need for sharp since we don't need img optimization

--- src/ui/next.config.ts
+++ src/ui/next.config.ts
@@ -25,6 +25,13 @@ const nextConfig: NextConfig = {
         use: ['@svgr/webpack'],
       },
       {
+        test: /\.(png|jpe?g|gif|webp|avif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'static/images/[name].[hash][ext]',
+        },
+      },
+      {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
         type: 'asset/resource',
         generator: {
