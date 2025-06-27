018345b1c720f91cb896303b4b5910049aa7d1d2
Samuel Monson <smonson@redhat.com>
2025-06-27 14:39:36 -0400

    Initial multiturn scheduler and types

---
+++ src/guidellm/request/session.py
@@ -0,0 +1,63 @@
+import itertools
+from abc import ABC, abstractmethod
+from typing import Generic, TypeVar
+
+from guidellm.request.request import GenerationRequest
+
+__all__ = ["RequestSession"]
+
+# TODO: Replace with specific types that implement needed features
+RequestT = TypeVar("RequestT")
+ResponseT = TypeVar("ResponseT")
+
+
+class RequestSession(ABC, Generic[RequestT, ResponseT]):
+    @abstractmethod
+    def get_next_request(self) -> RequestT: ...
+
+    @abstractmethod
+    def get_next_delay(self) -> float: ...
+
+    @abstractmethod
+    def push_response(self, response: ResponseT) -> None: ...
+
+    @property
+    @abstractmethod
+    def complete(self) -> bool: ...
+
+
+# FIXME: Dummy implementation
+class GenerativeRequestSession(RequestSession[GenerationRequest, str]):
+    def __init__(self, prompts: list[GenerationRequest]) -> None:
+        if not prompts:
+            raise ValueError("Prompts cannot be empty")
+
+        self.prompts = prompts
+        self.responses: list[str] = []
+
+    def get_request(self) -> GenerationRequest:
+        completed_responses = len(self.responses)
+        base_request = self.prompts[completed_responses].model_copy()
+        base_request.content = "".join(
+            itertools.chain.from_iterable(
+                zip((x.content for x in self.prompts), self.responses)
+            )
+        )
+        base_request.stats["prompt_tokens"] = sum(
+            x.stats["prompt_tokens"] for x in self.prompts[: completed_responses + 1]
+        )
+        base_request.constraints["output_tokens"] = sum(
+            x.constraints["output_tokens"] for x in self.prompts[:completed_responses]
+        )
+
+        return base_request
+
+    def push_response(self, response: str) -> None:
+        if len(self.responses) < len(self.prompts):
+            self.responses.append(response)
+        else:
+            raise ValueError("Response list full")
+
+    @property
+    def complete(self) -> bool:
+        return len(self.responses) >= len(self.prompts)
--- src/guidellm/scheduler/types.py
+++ src/guidellm/scheduler/types.py
@@ -1,7 +1,43 @@
-from typing import TypeVar
+from dataclasses import dataclass
+from multiprocessing import Queue
+from typing import Generic, Literal, Optional, TypeVar
 
-__all__ = ["RequestT", "ResponseT"]
+from guidellm.request.session import RequestSession
+from guidellm.scheduler.result import SchedulerRequestInfo
+
+__all__ = [
+    "MPQueues",
+    "RequestT",
+    "ResponseT",
+    "WorkerProcessRequestTime",
+    "WorkerProcessResult",
+]
 
 
 RequestT = TypeVar("RequestT")
 ResponseT = TypeVar("ResponseT")
+
+
+# TODO: Move dataclasses somewhere else
+
+
+@dataclass
+class WorkerProcessRequestTime:
+    start_time: float
+    timeout_time: float
+    queued_time: float
+
+
+@dataclass
+class WorkerProcessResult(Generic[RequestT, ResponseT]):
+    type_: Literal["request_scheduled", "request_start", "request_complete"]
+    request: RequestT
+    response: Optional[ResponseT]
+    info: SchedulerRequestInfo
+
+
+@dataclass
+class MPQueues(Generic[RequestT, ResponseT]):
+    requests: Queue[RequestSession[RequestT, ResponseT]]
+    times: Queue[WorkerProcessRequestTime]
+    responses: Queue[WorkerProcessResult[RequestT, ResponseT]]
